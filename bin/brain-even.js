04b15cde4acc5c7ac1e7e8837cd0b5d050ff09c7
Valeria Zherditskaia <st.leron@mail.ru>
2025-06-23 10:33:09 +0300

    feat: добавить игру brain-even для проверки на чётность
    - Создан исполняемый файл bin/brain-even.js
    - Реализована логика игры в файле src/brain-even.js
    - Обновлён файл package.json (секция bin)
    - Проведена отладка публикации
    - Записана аскинема с примером установки пакета и запуском игры
    - Ссылка на запись добавлена в README.md

---
+++ bin/brain-even.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import { parityCheck } from "../src/brain-even.js";
+
+parityCheck();
--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,18 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import { defineConfig } from 'eslint/config'
-import stylistic from '@stylistic/eslint-plugin'
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import stylistic from "@stylistic/eslint-plugin";
+import prettier from "eslint-config-prettier";
 
 export default defineConfig([
   {
-    files: ['**/*.{js,mjs,cjs}'],
+    files: ["**/*.{js,mjs,cjs}"],
     plugins: {
-      '@eslint/js': js,
-      '@stylistic': stylistic,
+      "@eslint/js": js,
+      "@stylistic": stylistic,
     },
     extends: [js.configs.recommended, stylistic.configs.recommended],
     languageOptions: { globals: globals.node },
   },
-])
+  prettier,
+]);
